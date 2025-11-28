a592a2d7dd36e5484a6c8ea19f69255e106e32f2
DenisZhmakin <den.zhmakin.00@gmail.com>
2025-11-28 11:21:54 +0300

    feat: Переименовал мод.

--- AlwaysSpawnSomeStaticContainers/AlwaysSpawnSomeStaticContainersExtension.cs
+++ AlwaysSpawnSomeStaticContainers/AlwaysSpawnSomeStaticContainersExtension.cs
@@ -3,10 +3,9 @@ using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Common;
-using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Servers;
 
-namespace AlwaysSpawnContainers.NG;
+namespace AlwaysSpawnSomeStaticContainers;
 
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
 public class AlwaysSpawnContainersExtension(ModHelper modHelper, DatabaseServer databaseServer) : IOnLoad
